d23e7aaf64fb10056a6d338552439e4a248af13c
hamyy2912 <74626106+hamyy2912@users.noreply.github.com>
2022-03-13 22:23:17 +0700

    add messages

---
+++ src/scripts/constants/messages.js
@@ -0,0 +1,26 @@
+/**
+ * MESSAGES
+ */
+
+// Confirm messages
+export const DELETE_TASK_CONFIRM = 'Are you sure you want to delete this task?';
+export const DELETE_ALL_TASKS_CONFIRM = 'Are you sure you want to delete all tasks?';
+export const DELETE_FINISHED_TASKS_CONFIRM = 'Are you sure you want to delete all finished tasks?';
+export const SWITCH_TASK_CONFIRM = 'Do you want to switch to another task?';
+
+export const RESET_TIMER_CONFIRM = 'Do you want to reset the timer?';
+export const FINISH_TIMER_CONFIRM = 'Do you want to finish this interval early?';
+
+// Warning messages
+export const INCORRECT_PASSWORD_WARN = 'Incorrect password';
+export const SHORT_PASSWORD_WARN = 'Password length must be greater than 4';
+export const INCORRECT_EMAIL_WARN = 'Please enter correct form of email address';
+export const EXISTENT_EMAIL_WARN = 'This email address is already registered. Please use another address.';
+export const NONEXISTENT_EMAIL_WARN = 'This email is not registered yet. Please create an account';
+export const MARK_AS_DONE_WARN = 'Cancel marking task done before edit task by clicking the check mark';
+
+// Success message
+export const SIGNUP_SUCCESSFULLY = 'Signup successfully. You can login now.';
+
+// Error message
+export const ERROR_MSG = 'Sorry! Something went wrong: ';
